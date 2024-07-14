2f5f43e8979acb1a024bbc6a07d23c5fa3913ad9
Bilenie Tilahun <130017473+Bilenie@users.noreply.github.com>
2024-07-13 23:11:07 -0500

    Added Url to the carousel images.

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,11 @@ document.addEventListener('DOMContentLoaded', function () {
         'https://media.licdn.com/dms/image/C4E1BAQFhp-ipaO0soA/company-background_10000/0/1584507505377/sunshine_daycare_center_cover?e=2147483647&v=beta&t=p_92rEz8h1Xy54XUiSXLAuBCL5vIGnn2iHAEvA0MWeY',
         'https://wpcdn.us-east-1.vip.tn-cloud.net/www.pittsburghparent.com/content/uploads/data-import/6e99a385/daycare-1050x700.jpg',
         'https://www.naeyc.org/sites/default/files/styles/page_header_media_large/public/102017/Public%20Policy%20Overview_2.jpg?itok=0f0oHaix',
-        'https://winnie.imgix.net/54eaa4aa-b08a-44aa-9a35-9e1283c72e36?w=242&h=124&dpr=3&fit=crop&auto=compress'
+        'https://winnie.imgix.net/54eaa4aa-b08a-44aa-9a35-9e1283c72e36?w=242&h=124&dpr=3&fit=crop&auto=compress',
+        'https://static1.squarespace.com/static/558c39a6e4b04cda071fb9fa/558d4e6ce4b04885fa76863f/5921978f29687fe45e717cb0/1495373722835//img.jpg',
+        'https://lh4.ggpht.com/-2fUuLw7-fhw/T39nkzfp8OI/AAAAAAAARYI/TsW-PK2Lb-0/image_thumb%25255B1%25255D.png?imgmax=800'
+
+
     ];
 
     // Initializes the currentIndex variable to keep track of the currently displayed image.
